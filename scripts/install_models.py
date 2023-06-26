e4ad501d9a3bf46a22a0f3db155d537117ffc1bc
Matthew Bachtell <mbachtell@users.noreply.github.com>
2023-06-26 06:37:11 -0400

    Removed EN to Other
    Removed the English to "foreign" language and only kept the "foreign" to English models.

--- scripts/install_models.py
+++ scripts/install_models.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--load_only_lang_codes", type=str, default="")
     args = parser.parse_args()
-    lang_codes = args.load_only_lang_codes.split(",")
+    #lang_codes = args.load_only_lang_codes.split(",")
+    lang_codes = ["ar","az","ca","zh","cs","da","nl","eo","fi","fr","de","el","he","hi","hu","id","ga","it","ja","ko","fa","pl","pt","ru","sk","es","sv","tr","uk"]
     if len(lang_codes) == 0 or lang_codes[0] == '':
         lang_codes = None
     check_and_install_models(force=True, load_only_lang_codes=lang_codes)
